84ccc3954f259fc80017fd33c1c8fc2779fb99c1
Cheryl Porter <cporter@ufl.edu>
2017-09-21 11:40:49 -0400

    Name change for single farm b&w plots

--- figure_one_farm_USA1.R
+++ figure_one_farm_USA1.R
@@ -1,4 +1,4 @@
-#Script to display whiskert plots for a random farm 
+#Script to display whiskert plots for one farm 
 #variables:
 # - Harvested yield (kg/ha)
 # - Total biomass (kg/ha)
@@ -10,51 +10,35 @@ rm(list=ls(all=TRUE))
 
 #reading table
 #x<-read.csv("D:/2017/CGRA data/from_cheryl/Colorado/summary_Maize/Summary_Maize_CM2.csv",header=TRUE)
-x<-read.csv("summary_Colorado_maize.csv",header=TRUE)
+x<-read.csv("summary_Camilla_peanut.csv",header=TRUE)
 #x<-read.csv("D:/2017/CGRA data/from_cheryl/Ames/summary_Soybean/Summary_Soybean_CM2.csv",header=TRUE)
-head(x)
 
 ##weather station code
 b<-unique(x$WSTA)
-b
 
 ##farm code
 c<-unique(x$TNAM)
-c
 
-#######select select a random farm 
-#### random farm for maize
-tnam<-c[1]
-#tnam<-"Clarion Loam"###for Ames soybean
-#tnam<-"Generic"         ###for Colorado
-#tnam<-"PT normal residue"         ###for Colorado###highly important
+#### for Camilla, use trt 6
+tnam <- as.character(c[6])
+####
 
-tnam
-#### random farm for soybean
-#tnam<-"UABN0001"
+x2 <- subset(x, TNAM == tnam, select=c(TNAM, WSTA, PDAT, ADAT, MDAT, HWAM, CWAM, TMAXA, TMINA, PRCP))
 
-##calculating the number of days for flowering (nday_flo) and maturity (nday_mat)
-#pdat<-as.numeric(substring(x$PDAT[x$TNAM==tnam],5,8))
-#adat<-as.numeric(substring(x$ADAT[x$TNAM==tnam],5,8))
-#hdat<-as.numeric(substring(x$HDA[x$TNAM==tnam],5,8))
-
-pyear <- floor(x$PDAT / 1000)  # truncate
-pdoy <- x$PDAT %% 1000         # modulo function
+pyear <- floor(x2$PDAT / 1000)  # truncate
+pdoy <- x2$PDAT %% 1000         # modulo function
 pdat <- ISOdate(pyear,12,31) + pdoy * 24 * 3600
 
-ayear <- floor(x$ADAT / 1000)
-adoy <- x$ADAT %% 1000
+ayear <- floor(x2$ADAT / 1000)
+adoy <- x2$ADAT %% 1000
 adat <- ISOdate(ayear,12,31) + adoy * 24 * 3600
 
-myear <- floor(x$MDAT / 1000)
-mdoy <- x$MDAT %% 1000
+myear <- floor(x2$MDAT / 1000)
+mdoy <- x2$MDAT %% 1000
 mdat <- ISOdate(myear,12,31) + mdoy * 24 * 3600
 
-nday_flo <- as.numeric(adat - pdat)
-nday_mat <- as.numeric(mdat - pdat)
-head(nday_flo)
-head(nday_mat)
-
+x2$nday_flo <- as.numeric(adat - pdat)
+x2$nday_mat <- as.numeric(mdat - pdat)
 
 ###################################
 
@@ -64,7 +48,7 @@ cex_lab=0.95
 ###################################
 
 #####to export the figure as a file
-dir<-"Summary_Colorado_maize.jpg"
+dir<-"Summary_Camilla_peanut_growth.jpg"
 jpeg(dir,res=300, width=7, height=11, unit="in")
 #####
 
@@ -82,13 +66,13 @@ par (mfrow=c(4,1),mar=c(0,4,0,1),oma=c(6,2,2,1),mgp = c(0,0.2, 0))
 #############################
 
 boxplot(
-  x$HWAM~x$WSTA, 
+  x2$HWAM~x2$WSTA, 
   xaxt="n",
   yaxt="n",
   las=1,
   at=c(1,2,3,4,5,6, 8,9,10,11,12), 
   col=c("gray", "green","red", "blue","yellow", "pink", "green","red", "blue","yellow", "pink"))#, 
-  outline=F)
+#  outline=F
 #axis(1, at=1:11,labels=c("OXFX","V4XF","V5XF","V8XF","VOXF","VTXF","W4XF","W5XF","W8XF","WOXF","WTXF"), tck=0.01,las=2,cex.axis=1)#AN00021
 #axis(1, at=1:12,labels=c("Base","CanAM4","CAM4","HadAM3P"," MIROC5","NorESM1", "","CanAM4","CAM4","HadAM3P"," MIROC5","NorESM1"), tck=0.01,las=2,cex.axis=1)
 axis(1, at=1:11, tck=0.01,labels=FALSE,las=2,cex.axis=cex)
@@ -97,7 +81,7 @@ abline(v=c(7),lty=3, col="black")
 mtext("Harv. yield (kg/ha)", side = 2, line = 4, outer = FALSE, at = NA,
       adj = NA, padj = NA, cex =cex_lab, col = NA, font = NA)
 
-boxplot(x$CWAM~x$WSTA, xaxt="n",yaxt="n",las=1,las=1,at=c(1,2,3,4,5,6, 8,9,10,11,12),col=c("gray", "green","red", "blue","yellow", "pink", "green","red", "blue","yellow", "pink"))
+boxplot(x2$CWAM~x2$WSTA, xaxt="n",yaxt="n",las=1,las=1,at=c(1,2,3,4,5,6, 8,9,10,11,12),col=c("gray", "green","red", "blue","yellow", "pink", "green","red", "blue","yellow", "pink"))
 #axis(1, at=1:11,labels=c("OXFX","V4XF","V5XF","V8XF","VOXF","VTXF","W4XF","W5XF","W8XF","WOXF","WTXF"), tck=0.01,las=2,cex.axis=1)
 #axis(1, at=1:12,labels=c("Base","CanAM4","CAM4","HadAM3P"," MIROC5","NorESM1", "","CanAM4","CAM4","HadAM3P"," MIROC5","NorESM1"), tck=0.01,las=2,cex.axis=1)
 axis(1, at=1:11, tck=0.01,labels=FALSE,las=2,cex.axis=cex)
@@ -106,7 +90,7 @@ abline(v=c(7),lty=3, col="black")
 mtext("Biomass (kg/ha)", side = 2, line = 4, outer = FALSE, at = NA,
       adj = NA, padj = NA, cex =cex_lab, col = NA, font = NA)
 
-boxplot(nday_flo~x$WSTA, xaxt="n",yaxt="n",las=1,las=1,at=c(1,2,3,4,5,6, 8,9,10,11,12),col=c("gray", "green","red", "blue","yellow", "pink", "green","red", "blue","yellow", "pink"))
+boxplot(x2$nday_flo~x2$WSTA, xaxt="n",yaxt="n",las=1,las=1,at=c(1,2,3,4,5,6, 8,9,10,11,12),col=c("gray", "green","red", "blue","yellow", "pink", "green","red", "blue","yellow", "pink"))
 #axis(1, at=1:11,labels=c("OXFX","V4XF","V5XF","V8XF","VOXF","VTXF","W4XF","W5XF","W8XF","WOXF","WTXF"), tck=0.01,las=2,cex.axis=1)
 #axis(1, at=1:12,labels=c("Base","CanAM4","CAM4","HadAM3P"," MIROC5","NorESM1", "","CanAM4","CAM4","HadAM3P"," MIROC5","NorESM1"), tck=0.01,las=2,cex.axis=1)
 axis(1, at=1:11, tck=0.01,labels=FALSE,las=2,cex.axis=cex)
@@ -115,7 +99,7 @@ abline(v=c(7),lty=3, col="black")
 mtext("Days to flowering", side = 2, line = 4, outer = FALSE, at = NA,
       adj = NA, padj = NA, cex =cex_lab, col = NA, font = NA)
 
-boxplot(nday_mat~x$WSTA, xaxt="n",yaxt="n",las=1,las=1,at=c(1,2,3,4,5,6, 8,9,10,11,12),col=c("gray", "green","red", "blue","yellow", "pink", "green","red", "blue","yellow", "pink"))
+boxplot(x2$nday_mat~x2$WSTA, xaxt="n",yaxt="n",las=1,las=1,at=c(1,2,3,4,5,6, 8,9,10,11,12),col=c("gray", "green","red", "blue","yellow", "pink", "green","red", "blue","yellow", "pink"))
 #axis(1, at=1:11,labels=c("OXFX","V4XF","V5XF","V8XF","VOXF","VTXF","W4XF","W5XF","W8XF","WOXF","WTXF"), tck=0.01,las=2,cex.axis=1)
 axis(1, at=1:12,labels=c("Base","CanAM4","CAM4","HadAM3P"," MIROC5","NorESM1", "","CanAM4","CAM4","HadAM3P"," MIROC5","NorESM1"), tck=0.01,las=2,cex.axis=1)
 #axis(1, at=1:11, tck=0.01,labels=FALSE,las=2,cex.axis=cex)
@@ -129,15 +113,15 @@ dev.off()
 ##to export the figure
 
 #####to export the figure as a file
-dir<-"Summary_Colorado_maize_weather.jpg"
-jpeg(dir,res=300, width=7, height=11, unit="in")
+dir<-"Summary_Camilla_peanut_weather.jpg"
+jpeg(dir,res=300, width=7, height=7, unit="in")
 #####
 
 #number of panels in a figure
 par (mfrow=c(3,1),mar=c(0,4,0,1),oma=c(6,2,2,1),mgp = c(0,0.2, 0))
 #number of panels in a figure
 
-boxplot(x$TMAXA[x$TNAM==tnam]~x$WSTA[x$TNAM==tnam], xaxt="n",yaxt="n",las=1,las=1,at=c(1,2,3,4,5,6, 8,9,10,11,12),col=c("gray", "green","red", "blue","yellow", "pink", "green","red", "blue","yellow", "pink"))#, outline=F,las=1,xaxt="n",yaxt="n")
+boxplot(x2$TMAXA~x2$WSTA, xaxt="n",yaxt="n",las=1,las=1,at=c(1,2,3,4,5,6, 8,9,10,11,12),col=c("gray", "green","red", "blue","yellow", "pink", "green","red", "blue","yellow", "pink"))#, outline=F,las=1,xaxt="n",yaxt="n")
 #axis(1, at=1:11,labels=c("OXFX","V4XF","V5XF","V8XF","VOXF","VTXF","W4XF","W5XF","W8XF","WOXF","WTXF"), tck=0.01,las=2,cex.axis=1)
 #axis(1, at=1:12,labels=c("Base","CanAM4","CAM4","HadAM3P"," MIROC5","NorESM1", "","CanAM4","CAM4","HadAM3P"," MIROC5","NorESM1"), tck=0.01,las=2,cex.axis=1)
 axis(1, at=1:11, tck=0.01,labels=FALSE,las=2,cex.axis=cex)
@@ -146,7 +130,7 @@ abline(v=c(7),lty=3, col="black")
 mtext("Seas. Tmax (°C)", side = 2, line = 4, outer = FALSE, at = NA,
       adj = NA, padj = NA, cex =cex_lab, col = NA, font = NA)
 
-boxplot(x$TMINA[x$TNAM==tnam]~x$WSTA[x$TNAM==tnam], xaxt="n",yaxt="n",las=1,las=1,at=c(1,2,3,4,5,6, 8,9,10,11,12),col=c("gray", "green","red", "blue","yellow", "pink", "green","red", "blue","yellow", "pink"))#, outline=F,las=1,xaxt="n",yaxt="n")
+boxplot(x2$TMINA~x2$WSTA, xaxt="n",yaxt="n",las=1,las=1,at=c(1,2,3,4,5,6, 8,9,10,11,12),col=c("gray", "green","red", "blue","yellow", "pink", "green","red", "blue","yellow", "pink"))#, outline=F,las=1,xaxt="n",yaxt="n")
 #axis(1, at=1:11,labels=c("OXFX","V4XF","V5XF","V8XF","VOXF","VTXF","W4XF","W5XF","W8XF","WOXF","WTXF"), tck=0.01,las=2,cex.axis=1)
 #axis(1, at=1:12,labels=c("Base","CanAM4","CAM4","HadAM3P"," MIROC5","NorESM1", "","CanAM4","CAM4","HadAM3P"," MIROC5","NorESM1"), tck=0.01,las=2,cex.axis=1)
 axis(1, at=1:11, tck=0.01,labels=FALSE,las=2,cex.axis=cex)
@@ -155,7 +139,7 @@ abline(v=c(7),lty=3, col="black")
 mtext("Seas. Tmin (°C)", side = 2, line = 4, outer = FALSE, at = NA,
       adj = NA, padj = NA, cex =cex_lab, col = NA, font = NA)
 
-boxplot(x$PRCP[x$TNAM==tnam]~x$WSTA[x$TNAM==tnam], xaxt="n",yaxt="n",las=1,las=1,at=c(1,2,3,4,5,6, 8,9,10,11,12),col=c("gray", "green","red", "blue","yellow", "pink", "green","red", "blue","yellow", "pink"))#, outline=F,las=1,xaxt="n",yaxt="n")
+boxplot(x2$PRCP~x2$WSTA, xaxt="n",yaxt="n",las=1,las=1,at=c(1,2,3,4,5,6, 8,9,10,11,12),col=c("gray", "green","red", "blue","yellow", "pink", "green","red", "blue","yellow", "pink"))#, outline=F,las=1,xaxt="n",yaxt="n")
 #axis(1, at=1:11,labels=c("OXFX","V4XF","V5XF","V8XF","VOXF","VTXF","W4XF","W5XF","W8XF","WOXF","WTXF"), tck=0.01,las=2,cex.axis=1)
 axis(1, at=1:12,labels=c("Base","CanAM4","CAM4","HadAM3P"," MIROC5","NorESM1", "","CanAM4","CAM4","HadAM3P"," MIROC5","NorESM1"), tck=0.01,las=2,cex.axis=cex)
 axis(2, tck=0.01,las=1,cex.axis=cex)
